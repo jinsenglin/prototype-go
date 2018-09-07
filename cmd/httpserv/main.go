39303ff187c7bf663ef8c423ee07482da10cda07
Jim Lin <jimlintw922@gmail.com>
2018-09-07 09:55:08 +0800

    mod httpserv

--- cmd/httpserv/main.go
+++ cmd/httpserv/main.go
@@ -9,18 +9,18 @@ import (
 
 func main() {
 	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-		if r.Method == "GET" {
+		if r.Method == http.MethodGet {
 			// HTTP GET to visit welcome page
 			// The "/" pattern matches everything, so we need to check
 			// that we're at the root here.
 			if r.URL.Path != "/" {
-				w.WriteHeader(404)
+				w.WriteHeader(http.StatusNotFound)
 				fmt.Fprintf(w, "Page Not Found")
 			} else {
 				fmt.Fprintf(w, "Welcome")
 			}
 		} else {
-			w.WriteHeader(405)
+			w.WriteHeader(http.StatusMethodNotAllowed)
 			fmt.Fprintf(w, "Method Not Allowed")
 		}
 	})
@@ -32,51 +32,51 @@ func main() {
 		if r.URL.Path == "/users/" {
 			// /users will be redirected to /users/
 
-			if r.Method == "GET" {
+			if r.Method == http.MethodGet {
 				fmt.Fprintf(w, "TODO: return a list of users")
-			} else if r.Method == "POST" {
+			} else if r.Method == http.MethodPost {
 				fmt.Fprintf(w, "TODO: create a user")
 			} else {
-				w.WriteHeader(405)
+				w.WriteHeader(http.StatusMethodNotAllowed)
 				fmt.Fprintf(w, "Method Not Allowed")
 			}
 		} else if r.URL.Path == "/users/new" {
-			if r.Method == "GET" {
+			if r.Method == http.MethodGet {
 				fmt.Fprintf(w, "TODO: return a form for user creation")
 			} else {
-				w.WriteHeader(405)
+				w.WriteHeader(http.StatusMethodNotAllowed)
 				fmt.Fprintf(w, "Method Not Allowed")
 			}
 		} else if re, _ := regexp.Compile("^/users/[1-9]$"); re.MatchString(r.URL.Path) {
-			if r.Method == "GET" {
+			if r.Method == http.MethodGet {
 				fmt.Fprintf(w, "TODO: return a user")
 			} else {
-				w.WriteHeader(405)
+				w.WriteHeader(http.StatusMethodNotAllowed)
 				fmt.Fprintf(w, "Method Not Allowed")
 			}
 		} else if re, _ := regexp.Compile("^/users/[1-9]/edit$"); re.MatchString(r.URL.Path) {
-			if r.Method == "GET" {
+			if r.Method == http.MethodGet {
 				fmt.Fprintf(w, "TODO: return a form for user modification")
 			} else {
-				w.WriteHeader(405)
+				w.WriteHeader(http.StatusMethodNotAllowed)
 				fmt.Fprintf(w, "Method Not Allowed")
 			}
 		} else if re, _ := regexp.Compile("^/users/[1-9]/update$"); re.MatchString(r.URL.Path) {
-			if r.Method == "PUT" {
+			if r.Method == http.MethodPut {
 				fmt.Fprintf(w, "TODO: update a user")
 			} else {
-				w.WriteHeader(405)
+				w.WriteHeader(http.StatusMethodNotAllowed)
 				fmt.Fprintf(w, "Method Not Allowed")
 			}
 		} else if re, _ := regexp.Compile("^/users/[1-9]/delete$"); re.MatchString(r.URL.Path) {
-			if r.Method == "DELETE" {
+			if r.Method == http.MethodDelete {
 				fmt.Fprintf(w, "TODO: delete a user")
 			} else {
-				w.WriteHeader(405)
+				w.WriteHeader(http.StatusMethodNotAllowed)
 				fmt.Fprintf(w, "Method Not Allowed")
 			}
 		} else {
-			w.WriteHeader(404)
+			w.WriteHeader(http.StatusNotFound)
 			fmt.Fprintf(w, "Page Not Found")
 		}
 	})
