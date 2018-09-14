3a1a3aceea625fbfcef098d66ed2c7ec82251a1f
cclin <jimlintw922@gmail.com>
2018-09-14 18:36:13 +0800

    mod chats.go

--- pkg/http/handler/chats.go
+++ pkg/http/handler/chats.go
@@ -2,14 +2,23 @@ package handler
 
 import (
 	"fmt"
+	"log"
 	"net/http"
 )
 
 func ChatsAPIHandler(w http.ResponseWriter, r *http.Request) {
-	if r.URL.Path == "/chats/new" {
+	if r.URL.Path == "/chats/" {
+		if r.Method == http.MethodPost {
+			log.Printf("TODO: add a chat '%s' to a channel", r.FormValue("chat")) // TODO
+			http.Redirect(w, r, "/chats/new", 301)
+		} else {
+			w.WriteHeader(http.StatusMethodNotAllowed)
+			fmt.Fprintf(w, "Method Not Allowed")
+		}
+	} else if r.URL.Path == "/chats/new" {
 		if r.Method == http.MethodGet {
 			w.Header().Set("Content-Type", "text/html; charset=utf-8")
-			fmt.Fprintf(w, "<form>chat: <input /><button>Submit</button></form>") // TODO: fix form action and method.
+			fmt.Fprintf(w, "<form action='/chats/' method='POST'>chat: <input name='chat'/><button>Submit</button></form>") // TODO: fix form action and method.
 		} else {
 			w.WriteHeader(http.StatusMethodNotAllowed)
 			fmt.Fprintf(w, "Method Not Allowed")
