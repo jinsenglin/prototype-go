6e72e87cbb33cef3a580dad6c5262ccbbf90db62
cclin <jimlintw922@gmail.com>
2018-09-14 19:38:24 +0800

    add comment

--- pkg/http/handler/chats.go
+++ pkg/http/handler/chats.go
@@ -13,7 +13,7 @@ func ChatsAPIHandler(w http.ResponseWriter, r *http.Request) {
 
 			_channel := channels.Items[0] // TODO: use the real channel.
 			_chats := _channel.Chats
-			_chats = append(_chats, model.Chat{Message: r.FormValue("chat")}) // TOOD: refactor with a chan.
+			_chats = append(_chats, model.Chat{Message: r.FormValue("chat")}) // TOOD: refactor with a chan for goroutine safe.
 
 			http.Redirect(w, r, "/chats/new", 301)
 		} else {
