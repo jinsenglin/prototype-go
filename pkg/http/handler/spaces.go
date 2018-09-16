56a017a21818795f9061cb7113bd4efbea0e94c2
cclin <jimlintw922@gmail.com>
2018-09-16 16:59:51 +0800

    mod spaces handler

--- pkg/http/handler/spaces.go
+++ pkg/http/handler/spaces.go
@@ -2,6 +2,7 @@ package handler
 
 import (
 	"fmt"
+	"log"
 	"net/http"
 	"regexp"
 )
@@ -10,11 +11,12 @@ func SpacesAPIHandler(w http.ResponseWriter, r *http.Request) {
 	if re, _ := regexp.Compile("^/spaces/[1-9]$"); re.MatchString(r.URL.Path) {
 		if r.Method == http.MethodGet {
 			w.Header().Set("Content-Type", "text/html; charset=utf-8")
-			fmt.Fprintf(w, "<html></html>") // TODO
+			fmt.Fprintf(w, "<html><frameset rows='*,50px'><frame src='/channels/1/chats'><frame src='/chats/new'></frameset></html>") // TODO: fix channel id
 		} else {
 			http.Error(w, "Method Not Allowed", http.StatusMethodNotAllowed)
 		}
 	} else {
+		log.Println(r.URL.Path)
 		http.NotFound(w, r)
 	}
 }
