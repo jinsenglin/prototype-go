d182c0110baa637a7d1427dc533ebb97220a6286
cclin <jimlintw922@gmail.com>
2018-09-17 11:25:35 +0800

    refactor

--- cmd/httpserv/main.go
+++ cmd/httpserv/main.go
@@ -18,7 +18,7 @@ import (
 )
 
 func main() {
-	log.Println("Try `curl -v -X GET -L http://localhost:8080`")
+	log.Println("Try `curl -L http://localhost:8080`")
 	route.RegisterRoutes()
 	log.Fatal(http.ListenAndServe(":8080", nil)) // TODO: refactor with https. See httpsserv
 }
