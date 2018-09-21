4c8876ed6b982a36cdf786ff127eff96a814e324
cclin <jimlintw922@gmail.com>
2018-09-21 11:03:53 +0800

    refactor

--- cmd/go-tour/exercise/slices/main.go
+++ cmd/go-tour/exercise/slices/main.go
@@ -3,13 +3,10 @@ package main
 import "golang.org/x/tour/pic"
 
 func Pic(dx, dy int) [][]uint8 {
-	y := make([][]uint8, 0, dy)
+	y := make([][]uint8, dy, dy)
 	for i := 0; i < dy; i++ {
-		x := make([]uint8, 0, dx)
-		for j := 0; j < dx; j++ {
-			x = append(x, uint8(0))
-		}
-		y = append(y, x)
+		x := make([]uint8, dx, dx)
+		y[i] = x
 	}
 	return y
 }
