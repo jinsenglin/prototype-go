4fbfb98fd128dbf9a178d4778200ec948ebc8d69
cclin <jimlintw922@gmail.com>
2018-09-29 01:00:06 +0800

    mod 1m-console

--- cmd/1m-console/main.go
+++ cmd/1m-console/main.go
@@ -3,7 +3,10 @@ package main
 import (
 	"flag"
 	"fmt"
+	"io/ioutil"
+	"log"
 	"os"
+	"os/exec"
 )
 
 var (
@@ -29,9 +32,26 @@ func init() {
 	flag.Usage = usage
 }
 
+func gcloudContainerClustersCreate() error {
+	cmd := exec.Command("gcloud", "container", "clusters", "create") // TODO
+	cmdStdout, _ := cmd.StdoutPipe()
+	cmdStderr, _ := cmd.StderrPipe()
+	cmd.Start()
+	cmdStdoutBytes, _ := ioutil.ReadAll(cmdStdout)
+	cmdStderrBytes, _ := ioutil.ReadAll(cmdStderr)
+	log.Println("gcloud container clusters create")
+	log.Printf("STDOUT %s", cmdStdoutBytes)
+	log.Printf("STDERR %s", cmdStderrBytes)
+	err := cmd.Wait()
+	return err
+}
+
 func up() {
-	// TODO: launch a GKE cluster
+	// Launch a GKE cluster
 	// No sdk to do this. Use gcloud command-line tool instead.
+	if err := gcloudContainerClustersCreate(); err != nil {
+		log.Fatalln(err)
+	}
 
 	// TODO: wait GKE cluster ready to use
 
