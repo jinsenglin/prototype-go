c41335afbfa65399e8b1db7783198385ece30308
Jim Lin <jimlintw922@gmail.com>
2018-09-28 16:59:23 +0800

    mod 1m-console

--- cmd/1m-console/main.go
+++ cmd/1m-console/main.go
@@ -12,16 +12,20 @@ var (
 )
 
 func usage() {
-	fmt.Fprintf(os.Stderr, "Usage: %s [OPTIONS] URL\n\n", os.Args[0])
+	fmt.Fprintf(os.Stderr, "Usage: %s [OPTIONS] URL\n\n", os.Args[0]) // TODO: remove
 	fmt.Fprintln(os.Stderr, "OPTIONS:")
 	flag.PrintDefaults()
 	fmt.Fprintln(os.Stderr, "")
 	fmt.Fprintln(os.Stderr, "ENVIRONMENT:")
 	fmt.Fprintln(os.Stderr, " HTTP_PROXY proxy for HTTP requests; complete URL or HOST[:PORT]") // TODO: remove
+	fmt.Fprintln(os.Stderr, "")
+	fmt.Fprintln(os.Stderr, "SUBCOMMAND:")
+	fmt.Fprintln(os.Stderr, " launch ...") // TODO: remove
 }
 
 func init() {
 	flag.IntVar(&n, "num", 1, "number of consumer workers")
+	flag.NewFlagSet("launch", flag.ExitOnError)
 	flag.Usage = usage
 }
 
